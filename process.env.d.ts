dc8b64d5520000f0a7aab0d4b7a28d90b7f5928a
Androz2091 <androz2091@gmail.com>
2021-11-20 12:42:13 +0100

    :wrench: Fill env file

---
+++ process.env.d.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        DISCORD_CLIENT_TOKEN: string;
+
+        DB_NAME: string;
+        DB_USERNAME: string;
+        DB_PASSWORD: string;
+
+        EMBED_COLOR: string;
+    }
+}
