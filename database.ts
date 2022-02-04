9f6ab197c637aa473d84815a203e2cf14878bef8
Androz <androz2091@gmail.com>
2022-02-04 17:24:48 +0100

    :fire: Remove useless export of connection

--- database.ts
+++ database.ts
@@ -1,7 +1,5 @@
 import { Entity, Column, createConnection, Connection, PrimaryGeneratedColumn } from "typeorm";
 
-export let connection: Connection;
-
 export const initialize = () => createConnection({
     type: 'postgres',
     host: 'localhost',
@@ -10,7 +8,7 @@ export const initialize = () => createConnection({
     password: process.env.DB_PASSWORD,
     entities: [User],
     synchronize: process.env.ENVIRONMENT === 'development',
-}).then((createdConnection) => connection = createdConnection);
+});
 
 @Entity()
 export class User {
