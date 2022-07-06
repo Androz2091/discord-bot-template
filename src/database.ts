65e5eb6dd378232afaf78ca589418cd7b63089e7
Androz2091 <androz2091@gmail.com>
2022-07-06 17:27:41 +0200

    add new adminjs

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,30 @@
-import { Entity, Column, createConnection, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, DataSource, PrimaryGeneratedColumn, BaseEntity } from "typeorm";
+import express from 'express';
+import { Database, Resource } from '@adminjs/typeorm';
+import { validate } from 'class-validator';
 
-export const initialize = () => createConnection({
+import AdminJS from 'adminjs';
+import AdminJSExpress from '@adminjs/express';
+
+Resource.validate = validate;
+AdminJS.registerAdapter({ Database, Resource });
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({
+        length: 32
+    })
+    userId!: string;
+    
+    @Column()
+    money!: number;
+
+}
+
+export const Postgres = new DataSource({
     type: 'postgres',
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -10,16 +34,20 @@ export const initialize = () => createConnection({
     synchronize: process.env.ENVIRONMENT === 'development',
 });
 
-@Entity()
-export class User {
-    @PrimaryGeneratedColumn()
-    id!: number;
-
-    @Column({
-        length: 32
-    })
-    userId!: string;
-
-    @Column()
-    money!: number;
-}
+export const initialize = Postgres.initialize().then(() => {
+    if (process.env.ADMINJS_PORT) {
+        const app = express();
+        const admin = new AdminJS({
+            branding: {
+                
+            },
+            resources: [User],
+        })
+        const router = AdminJSExpress.buildRouter(admin)
+        app.use(admin.options.rootPath, router)
+        app.listen(process.env.ADMINJS_PORT, () => {
+            console.log(`AdminJS is listening at http://localhost:${process.env.ADMINJS_PORT}`)
+        });
+    }
+    
+});
