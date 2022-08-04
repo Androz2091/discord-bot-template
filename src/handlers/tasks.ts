df267dce37a29299049e99e9c81ded6019dea365
Simon <androz2091@gmail.com>
2022-08-04 11:41:40 +0200

    fix tasks handling

--- src/handlers/tasks.ts
+++ src/handlers/tasks.ts
@@ -20,6 +20,8 @@ export const loadTasks = (client: Client) => {
             if (file.endsWith('.js')) {
                 const task = require(join(__dirname, '..', 'tasks', file));
                 if (!task.crons) return console.log(`${file} has no task`);
+                
+                const taskName = file.split('.')[0];
 
                 task.jobs = [];
                 task.crons.forEach((cron: string) => {
@@ -28,9 +30,9 @@ export const loadTasks = (client: Client) => {
                     }, null, true, 'America/Los_Angeles'));
                 });
 
-                tasks.set(task.name, task);
+                tasks.set(taskName, task);
                 tasksData.push(task);
-                console.log(`Loaded task ${file.split('.')[0]}`);
+                console.log(`Loaded task ${taskName}`);
             }
         });
     } catch {
