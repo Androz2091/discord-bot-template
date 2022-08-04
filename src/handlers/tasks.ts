5b9f726814492571433cb551bcb6bbca558d15be
Simon <androz2091@gmail.com>
2022-08-04 11:37:33 +0200

    remove useless code in task handler

--- src/handlers/tasks.ts
+++ src/handlers/tasks.ts
@@ -4,66 +4,6 @@ import { readdirSync } from "fs";
 import { join } from "path";
 import { CronJob } from "cron";
 
-interface SynchronizeSlashCommandOptions {
-    guildId?: null|string;
-    debug?: boolean;
-}
-
-export const synchronizeSlashCommands = async (client: Client, commands: ChatInputApplicationCommandData[], options: SynchronizeSlashCommandOptions = {}) => {
-
-    const log = (message: string) => options.debug && console.log(message);
-
-    const ready = client.readyAt ? Promise.resolve() : new Promise(resolve => client.once('ready', resolve));
-    await ready;
-    const currentCommands = options.guildId ? await client.application!.commands.fetch({
-        guildId: options.guildId,
-    }) : await client.application!.commands.fetch();
-
-    log(`Synchronizing commands...`);
-    log(`Currently ${currentCommands.size} commands.`);
-
-    const newCommands = commands.filter((command) => !currentCommands.some((c) => c.name === command.name));
-    for (let newCommand of newCommands) {
-        if (options.guildId) await client.application!.commands.create(newCommand, options.guildId);
-        else await client.application!.commands.create(newCommand);
-    }
-
-    log(`Created ${newCommands.length} commands!`);
-
-    const deletedCommands = currentCommands.filter((command) => !commands.some((c) => c.name === command.name)).toJSON();
-    for (let deletedCommand of deletedCommands) {
-        await deletedCommand.delete();
-    }
-
-    log(`Deleted ${deletedCommands.length} commands!`);
-
-    const updatedCommands = commands.filter((command) => currentCommands.some((c) => c.name === command.name));
-    let updatedCommandCount = 0;
-    for (let updatedCommand of updatedCommands) {
-        const newCommand = updatedCommand;
-        const previousCommand = currentCommands.find((c) => c.name === updatedCommand.name);
-        let modified = false;
-        if (previousCommand?.description !== newCommand?.description) modified = true;
-        if (!ApplicationCommand.optionsEqual(previousCommand!.options ?? [], newCommand.options ?? [])) modified = true;
-        if (modified) {
-            await previousCommand!.edit(newCommand as unknown as ApplicationCommandData);
-            updatedCommandCount++;
-        }
-    }
-
-    log(`Updated ${updatedCommandCount} commands!`);
-
-    log(`Commands synchronized!`);
-
-    return {
-        currentCommandCount: currentCommands.size,
-        newCommandCount: newCommands.length,
-        deletedCommandCount: deletedCommands.length,
-        updatedCommandCount
-    };
-
-};
-
 export interface TaskData {
     name: string;
     crons: string;
