a0345b57d3f390970a97d964137ebc94f0dabfeb
Androz2091 <androz2091@gmail.com>
2022-11-10 20:58:20 +0100

    Add pagination

--- src/util.ts
+++ src/util.ts
@@ -33,3 +33,22 @@ export const replyEmbed = (message: string) => {
 export const generateId = () => {
     return Math.random().toString(36).slice(2, 12);
 }
+
+export const generateEmbeds = ({ entries, generateEmbed, generateEntry }: {
+    entries: any[],
+    generateEmbed: (idx: number) => EmbedBuilder,
+    generateEntry: (entry: any) => string
+}) => {
+    const embeds: EmbedBuilder[] = [];
+    entries.forEach((entry) => {
+        const entryContent = generateEntry(entry);
+        const lastEmbedTooLong = !embeds.length || (embeds.at(-1)!.data.description!.length + entryContent.length) >= 2048;
+        if (lastEmbedTooLong) {
+            const newEmbed = generateEmbed(embeds.length);
+            embeds.push(newEmbed);
+        }
+        const lastEmbed = embeds.at(-1)!;
+        lastEmbed.data.description += entryContent;
+    });
+    return embeds;
+};
